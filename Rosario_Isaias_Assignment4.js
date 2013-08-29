407054a70379ac3d1b5400c83da160a817112acd
r16902 <irosario@fullsail.edu>
2013-08-29 08:39:58 -0400

    Added myLib Library and chkPhnNum string Function

--- Rosario_Isaias_Assignment4.js
+++ Rosario_Isaias_Assignment4.js
@@ -1,1 +1,27 @@
-alert("JavaScript works!");
+// alert("JavaScript works!");
+
+// Isaias Rosario
+// SDI Term 1308
+// Project 4
+// Solving Problems
+
+// My Library of Functions 
+var myLib = function() {
+
+	var chkPhnNum = function(phnNum) {
+		if(phnNum.length === 12 && phnNum.substring(3, 4) && phnNum.substring(7, 8) === "-") {
+			return true;
+		} else {
+			return false;
+		};
+	};	
+	
+	return{
+		"chkPhnNum": chkPhnNum
+	};
+	
+}; // end of myLib
+
+var lib = myLib();
+
+console.log("Is this a phone number? " + lib.chkPhnNum("413-555-5555"));

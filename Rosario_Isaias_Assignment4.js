1006c17367cf4fca9633a4f7ec9d07baa4ccdd5e
r16902 <irosario@fullsail.edu>
2013-08-29 13:08:00 -0400

    Added Stub Function to myLib and a few blank console.logs
    Set this up to make it easier for me as I code.

--- Rosario_Isaias_Assignment4.js
+++ Rosario_Isaias_Assignment4.js
@@ -8,20 +8,68 @@
 // My Library of Functions 
 var myLib = function() {
 
+	// Check if String is a Phone Number
 	var chkPhnNum = function(phnNum) {
 		if(phnNum.length === 12 && phnNum.substring(3, 4) && phnNum.substring(7, 8) === "-") {
 			return true;
 		} else {
 			return false;
 		};
-	};	
+	};	// end of chkPhnNum function
+	
+	// Check if String is an Email
+	var chkEmail = function() {
+		if(){
+			return true;
+		} else {
+			return false;
+		};
+	
+	}; // end of chkEmail function
+	
+	// Check if String is has http:// or https://
+	var chkHttp = function() {
+		if(){
+			return true;
+		} else {
+			return false;
+		};
+		
+	}; // end of chkHttp function
+	
+	// Check if String is Title Case
+	var chkTiCase = function() {
+	
+	}; // end of chkTiCase function
+	
+	// Set Decimal Place for a Given Number
+	var setDecPlc = function() {
+	
+	}; // end of setDecPlc function
+	
+	// Check for Smallest Number in Array greater then Given Number 
+	var chkArrNum = function() {
+	
+	}; // end of chkArrNum  function
 	
 	return{
 		"chkPhnNum": chkPhnNum
+		"chkEmail" : chkEmail,
+		"chkHttp"  : chkHttp,
+		"chkTiCase": chkCase,
+		"setDecPlc": chkDecPlc,
+		"chkArrNum": chkArrNum
 	};
 	
 }; // end of myLib
 
+// Start //
+
 var lib = myLib();
 
 console.log("Is this a phone number? " + lib.chkPhnNum("413-555-5555"));
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
